d389701c207f13854e01555af18dd5ec5ed9a20f
AhmedNoureldinAhmed <m-32500912@s-mu.edu.eg>
2019-10-19 14:43:01 +0200

    FIX Repeation and Add interval from user

--- main.cpp
+++ main.cpp
@@ -1,50 +1,76 @@
 #include <iostream>
-#include <random>
-#include <ctime>
+#include <random>  // Random Function
+#include <ctime>   // Time Function
 
 
 using namespace std;
 
-int _random()
-{
+// initialized Start and End Point For Random Number
+size_t _start,_end;
 
-  // time(NULL) To Skip Replication
-  static std::default_random_engine e{time(NULL)};   // Define Engine Random
-  static std::uniform_int_distribution<int> d{0, 9};  // Define uniform integer Distribution
+int _random(size_t,size_t);
 
-return d(e);
- }
+unsigned* RandomSample(unsigned [],size_t,size_t);
 
- unsigned RandomSample(unsigned *population,size_t nPop,size_t nSample){
+void print(unsigned [],size_t );
 
-    unsigned sample[nSample]{};
-
- for(size_t i=0; i < nSample; i++ )
-   {
-
-      sample[i]=population[_random()]; /*
-
-       Assigned (From Population To Random Sample )
-
-       */
-
-  }
-  return *sample;
- }
 
 
 int main()
 {
 
- size_t sizeP{10},sizeS{5};
+ size_t sizeP{20},sizeS{10};
 
- unsigned  p [sizeP]{75,55,85,130,90,22,70,55,30,98}, sample[sizeS]{};
+ unsigned  p[sizeP]{1,2,99,4,5,6,7,8,9,10,11,112,13,14,15,16,17,18,19,200}, sample[sizeS]{};
 
-     for(size_t i=0; i < sizeS; i++ )
-   {
+ cout<<"Enter interval Random"<<endl<<"From : " ;
+ cin >>_start;
+ cout<<"To : ";
+ cin>>_end;
 
-      cout<<RandomSample(p,sizeP,sizeS)<<endl;
 
-  }
+ RandomSample(p,sizeP,sizeS);
 
 }
+
+
+int _random()
+{
+
+                    // From Random library
+  static std::default_random_engine e{time(NULL)};      // Define Engine Random
+  static std::uniform_int_distribution<int> d{(_start), (_end)};  // Define uniform integer Distribution
+                                                        //Note =>time(NULL) To Skip Replication Every Run
+    return d(e);
+ // Method (2) return (rand()%20+1);
+}
+
+
+
+ unsigned* RandomSample(unsigned population[],size_t nPop,size_t nSample){
+
+ unsigned sample[nSample]{}; // Define Empty Array to Take Samples
+
+            // Assigned Random Population to Sample
+ for(size_t i=0; i < nSample; i++ )
+    {
+            sample[i]=population[_random()];
+    }
+
+            // Catch Repeat Sample
+  for(size_t i=0; i < nSample; i++ )
+   {
+         for(size_t j=0; j < nSample; j++ )
+        {
+            if (i == j )  {continue;} // Skip Sample[0] == Sample [0] etc..
+            else { if (sample[i] == sample[j]) {sample[j] = population[_random()];}} // Replace RepeatSamples
+        }
+ }
+        // Print Sample
+     print(sample,nSample);
+
+
+  return sample;
+ }
+
+void print(unsigned sample[],size_t nSample){for(size_t i=0; i < nSample; i++ )  cout<<sample[i]<<endl;}
